ed3bae9805f7f936aa764bc536a52f578d7312b7
Aistis Raulinaitis <sheganinans@gmail.com>
2022-09-04 12:56:48 -0700

    bit at byte

---
+++ src/Example/BitOfByte.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
+module Example.BitOfByte (topEntity) where
+
+import Clash.Prelude
+
+topEntity ::
+  Clock System ->
+  Reset System ->
+  Enable System ->
+  Signal System (Unsigned 8) ->
+  Signal System Bool
+topEntity = exposeClockResetEnable bitOfByte
+
+bitAt :: Unsigned 3 -> Unsigned 8 -> Bool
+bitAt 0 $(bitPattern "1.......") = True
+bitAt 1 $(bitPattern ".1......") = True
+bitAt 2 $(bitPattern "..1.....") = True
+bitAt 3 $(bitPattern "...1....") = True
+bitAt 4 $(bitPattern "....1...") = True
+bitAt 5 $(bitPattern ".....1..") = True
+bitAt 6 $(bitPattern "......1.") = True
+bitAt 7 $(bitPattern ".......1") = True
+bitAt _ _ = False
+
+bitOfByte :: (HiddenClockResetEnable dom) => Signal dom (Unsigned 8) -> Signal dom Bool
+bitOfByte =
+  moore
+    (\_ inp -> inp)
+    (bitAt 4)
+    (0 :: Unsigned 8)
